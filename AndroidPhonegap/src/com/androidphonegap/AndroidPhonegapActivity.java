d584fab79ae7f8b444157dba5afd37b34b20be69
Mathieu Marmié <mathieu.marmie@viacesi.fr>
2013-02-19 10:37:21 +0100

    corrections bug projet appli smartphone participants

--- AndroidPhonegap/src/com/androidphonegap/AndroidPhonegapActivity.java
+++ AndroidPhonegap/src/com/androidphonegap/AndroidPhonegapActivity.java
@@ -7,13 +7,13 @@ import android.view.Menu;
 
 public class AndroidPhonegapActivity extends DroidGap {
 
-    @Override
+    
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/ecranOuverture.html");
     }
 
-    @Override
+   
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
