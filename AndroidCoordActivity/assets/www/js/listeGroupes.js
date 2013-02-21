09453500ec6e994872bcb84fe9a4e16d107cc1b5
Mathieu Marmié <mathieu.marmie@viacesi.fr>
2013-02-21 10:44:51 +0100

    suiviEquipes

---
+++ AndroidCoordActivity/assets/www/js/listeGroupes.js
@@ -0,0 +1,19 @@
+$('#suiviEquipes').live('pagebeforeshow', function(event, data) {
+	$.ajax({
+		  type: "GET",
+		  url: "http://bo16x221mobileg1.dx.am/service.php?type=listeGroupes&params=enquete*1",
+		  dataType: "json"
+		}).done(function(groupes) {
+			$('#tousGroupes li').remove();
+			
+			$.each(groupes, function(index, groupe){
+				$('#tousGroupes').append('<li id="'+groupe.idGroupe+'"><a href="#suiviEquipe" data-transition="slidefade" data-inline="true">' + groupe.nomGroupe +'</a><div>Numéro du groupe : '+ groupe.numeroGroupe +'</div><br/><div>Téléphone : '+ groupe.telephoneGroupe +'</div><br/><div>Score : '+ groupe.scoreGroupe +'</div><br/><div>Latitude : '+ groupe.latitudeGroupe +'</div><br/><div>Longitude : '+ groupe.longitudeGroupe +'</div><br/></li>');
+				
+			});
+			$('#tousGroupes').listview('refresh');
+		});
+
+});
+
+
+
