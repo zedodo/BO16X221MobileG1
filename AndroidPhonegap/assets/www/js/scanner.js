656f4b895f9be456c07a84795541d6ce99969f7e
Romain Larrouy <romain.larrouy@viacesi.fr>
2013-02-21 14:43:11 +0100

    nom appli

--- AndroidPhonegap/assets/www/js/scanner.js
+++ AndroidPhonegap/assets/www/js/scanner.js
@@ -1,12 +1,19 @@
+//alert('scan');
 $('#pageScanner').live('pagebeforeshow', function(event, data) {
 
+	//alert('scan');
 	window.plugins.barcodeScanner.scan( function(result) {
-	        alert("We got a barcode\n" +
+	        /*alert("We got a barcode\n" +
 	                  "Result: " + result.text + "\n" +
 	                  "Format: " + result.format + "\n" +
-	                  "Cancelled: " + result.cancelled);
+	                  "Cancelled: " + result.cancelled);*/
+	        
+	        $("#pageScannerContent").html("Vous avez scann&eacute; :<br/>" + 
+	        		"Resultat: " + result.text + "<br/>" +
+	        		"Format: " + result.format + "<br/>");
 	    }, function(error) {
-	        alert("Scanning failed: " + error);
+	        //alert("Echec du scan: " + error);
+	    	alert('Echec du scan');
 	    }
 	);
-}
+});
